747e067d59e0887385fc572595302bcb3f50dc6b
Juan Pablo Olivadoti Locatelli <juan.olivadoti@atmira.com>
2019-11-08 14:24:46 +0100

    Previo al proyecto

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './sever/server.component';
 import { ServersComponent } from './severs/servers.component';
+import { WarnigAlert } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarnigAlert,
+    SuccessAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
