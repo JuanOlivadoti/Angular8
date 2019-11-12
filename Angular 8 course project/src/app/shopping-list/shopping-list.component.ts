3566b6b1d6f2d9e85f6cd32880da45b8793b5294
Juan Pablo Olivadoti Locatelli <juan.olivadoti@atmira.com>
2019-11-12 13:36:16 +0100

    cmp-databinding

--- Angular 8 course project/src/app/shopping-list/shopping-list.component.ts
+++ Angular 8 course project/src/app/shopping-list/shopping-list.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients= [];
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomato', 10),
+    new Ingredient('Banana', 15)
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
-}
+};
